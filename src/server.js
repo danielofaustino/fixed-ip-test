cc7c3ed041ba6ece71dd85c1760f204ed2b1e818
Daniel Faustino <danieloliveirafaustino@gmail.com>
2021-08-02 09:23:47 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const router = require('./routes');
@@ -6,6 +7,7 @@ const router = require('./routes');
 const app = express();
 
 app.use(helmet());
+app.use(cors())
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(router);
